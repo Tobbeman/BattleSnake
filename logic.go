ce8f0abdcc9cd1f59eef361a72660f6c7873c3d7
Tobias Laving <tobias@trine.com>
2022-02-19 20:15:57 +0100

    Customise snake

--- logic.go
+++ logic.go
@@ -19,8 +19,8 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Personalize
+		Author:     "Tobbeman",
+		Color:      "#4C89C8",
 		Head:       "default", // TODO: Personalize
 		Tail:       "default", // TODO: Personalize
 	}
